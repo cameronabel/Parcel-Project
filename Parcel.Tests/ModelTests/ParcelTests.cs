40b278c6b5ad9c10ba0251aa9660a0d13221dace
Chloe ONeil <chloesporri@yahoo.com>
2023-02-14 19:28:18 -0800

    pass fifth test Co-authored-by: Geoffrey Gao <geoffreygao1@gmail.com> Co-authored-by: Cameron Abel <cameronabel@gmail.com>

--- Parcel.Tests/ModelTests/ParcelTests.cs
+++ Parcel.Tests/ModelTests/ParcelTests.cs
@@ -10,14 +10,14 @@ namespace Parcel.Tests
     [TestMethod]
     public void PackageConstructor_CreatesInstanceOfPackage_Package()
     {
-      Package newPackage = new Package(3, 4, 5);
+      Package newPackage = new Package(3, 4, 5, 10);
       Assert.AreEqual(typeof(Package), newPackage.GetType());
     }
     [TestMethod]
     public void GetLength_ReturnsLength_Int()
     {
       int length = 3;
-      Package newPackage = new Package(length, 4, 5);
+      Package newPackage = new Package(length, 4, 5, 10);
       int result = newPackage.Length;
       Assert.AreEqual(length, result);
     }
@@ -25,7 +25,7 @@ namespace Parcel.Tests
     public void GetWidth_ReturnsWidth_Int()
     {
       int width = 4;
-      Package newPackage = new Package(3, width, 5);
+      Package newPackage = new Package(3, width, 5, 10);
       int result = newPackage.Width;
       Assert.AreEqual(width, result);
     }
@@ -33,9 +33,17 @@ namespace Parcel.Tests
     public void GetHeight_ReturnsHeight_Int()
     {
       int height = 5;
-      Package newPackage = new Package(3, 4, height);
+      Package newPackage = new Package(3, 4, height, 10);
       int result = newPackage.Height;
       Assert.AreEqual(height, result);
+    }
+    [TestMethod]
+    public void GetWeight_ReturnsWeight_Int()
+    {
+      int weight = 10;
+      Package newPackage = new Package(3, 4, 5, weight);
+      int result = newPackage.Weight;
+      Assert.AreEqual(weight, result);
     }
   }
 }
