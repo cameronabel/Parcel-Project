9f924d3e1bad77fc9e7457483b87ab1ef12866ff
Chloe ONeil <chloesporri@yahoo.com>
2023-02-14 19:26:32 -0800

    pass fourth test Co-authored-by: Geoffrey Gao <geoffreygao1@gmail.com> Co-authored-by: Cameron Abel <cameronabel@gmail.com>

--- Parcel.Tests/ModelTests/ParcelTests.cs
+++ Parcel.Tests/ModelTests/ParcelTests.cs
@@ -10,14 +10,14 @@ namespace Parcel.Tests
     [TestMethod]
     public void PackageConstructor_CreatesInstanceOfPackage_Package()
     {
-      Package newPackage = new Package(3, 4);
+      Package newPackage = new Package(3, 4, 5);
       Assert.AreEqual(typeof(Package), newPackage.GetType());
     }
     [TestMethod]
     public void GetLength_ReturnsLength_Int()
     {
       int length = 3;
-      Package newPackage = new Package(length, 4);
+      Package newPackage = new Package(length, 4, 5);
       int result = newPackage.Length;
       Assert.AreEqual(length, result);
     }
@@ -25,9 +25,17 @@ namespace Parcel.Tests
     public void GetWidth_ReturnsWidth_Int()
     {
       int width = 4;
-      Package newPackage = new Package(3, width);
+      Package newPackage = new Package(3, width, 5);
       int result = newPackage.Width;
       Assert.AreEqual(width, result);
+    }
+    [TestMethod]
+    public void GetHeight_ReturnsHeight_Int()
+    {
+      int height = 5;
+      Package newPackage = new Package(3, 4, height);
+      int result = newPackage.Height;
+      Assert.AreEqual(height, result);
     }
   }
 }
