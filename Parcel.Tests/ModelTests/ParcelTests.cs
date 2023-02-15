d1c8f3649ecadfee5b9e8b1c70c63562116a3b80
Chloe ONeil <chloesporri@yahoo.com>
2023-02-14 19:23:43 -0800

    pass third test Co-authored-by: Geoffrey Gao <geoffreygao1@gmail.com> Co-authored-by: Cameron Abel <cameronabel@gmail.com>

--- Parcel.Tests/ModelTests/ParcelTests.cs
+++ Parcel.Tests/ModelTests/ParcelTests.cs
@@ -10,16 +10,24 @@ namespace Parcel.Tests
     [TestMethod]
     public void PackageConstructor_CreatesInstanceOfPackage_Package()
     {
-      Package newPackage = new Package(5);
+      Package newPackage = new Package(3, 4);
       Assert.AreEqual(typeof(Package), newPackage.GetType());
     }
     [TestMethod]
     public void GetLength_ReturnsLength_Int()
     {
       int length = 3;
-      Package newPackage = new Package(length);
+      Package newPackage = new Package(length, 4);
       int result = newPackage.Length;
       Assert.AreEqual(length, result);
+    }
+    [TestMethod]
+    public void GetWidth_ReturnsWidth_Int()
+    {
+      int width = 4;
+      Package newPackage = new Package(3, width);
+      int result = newPackage.Width;
+      Assert.AreEqual(width, result);
     }
   }
 }
