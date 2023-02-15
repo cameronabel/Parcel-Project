d9ffb5d8b28250696682438c2ae41ba758e4424a
Chloe ONeil <chloesporri@yahoo.com>
2023-02-14 19:19:43 -0800

    pass second test Co-authored-by: Geoffrey Gao <geoffreygao1@gmail.com> Co-authored-by: Cameron Abel <cameronabel@gmail.com>

--- Parcel.Tests/ModelTests/ParcelTests.cs
+++ Parcel.Tests/ModelTests/ParcelTests.cs
@@ -10,8 +10,16 @@ namespace Parcel.Tests
     [TestMethod]
     public void PackageConstructor_CreatesInstanceOfPackage_Package()
     {
-      Package newPackage = new Package();
+      Package newPackage = new Package(5);
       Assert.AreEqual(typeof(Package), newPackage.GetType());
+    }
+    [TestMethod]
+    public void GetLength_ReturnsLength_Int()
+    {
+      int length = 3;
+      Package newPackage = new Package(length);
+      int result = newPackage.Length;
+      Assert.AreEqual(length, result);
     }
   }
 }
