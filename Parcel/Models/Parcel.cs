0d2c715cc940f730f19a682b4f263e4d02271d70
Chloe ONeil <chloesporri@yahoo.com>
2023-02-14 19:35:12 -0800

    pass sixth test Co-authored-by: Geoffrey Gao <geoffreygao1@gmail.com> Co-authored-by: Cameron Abel <cameronabel@gmail.com>

---
+++ Parcel/Models/Parcel.cs
@@ -0,0 +1,28 @@
+namespace Parcel.Models
+{
+  public class Package
+  {
+    public int Length {get; set;}
+    public int Width {get; set;}
+    public int Height {get; set;}
+    public int Weight {get; set;}
+
+    public Package(int length, int width, int height, int weight)
+    {
+      Length = length;
+      Width = width;
+      Height = height;
+      Weight = weight;
+    }
+
+    public int Volume()
+    {
+      return Length*Width*Height;
+    }
+
+    public int CostToShip()
+    {
+      return 0;
+    }
+  }
+}
